0f1a3b4bb81b6cd3df95923b42a2b06d979f7299
ArvinRad <radanarvin@gmail.com>
2021-05-03 18:09:44 +0000

    Add image upload with middleware multer and message tables

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,1 +1,1 @@
-INSERT INTO profucts (name, description, price, stock, user_id, thumbnail, imge_id) VALUES ('shoes1', 'A par of nice shoes for lady', 43, 10, false, true, 1, 'www.picture.com/126172', 0);
+INSERT INTO products (name, description, price, stock, user_id, thumbnail, image_id) VALUES ('shoes1', 'A par of nice shoes for lady', 43, 10, false, true, 1, 'www.picture.com/126172', 0);
