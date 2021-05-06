90e5fb6d059bbd6ba22f1fb70ff2a3aa28f84d2f
karanOO89 <gk.987680@gmail.com>
2021-05-06 21:47:14 +0000

    filter options discussed

--- routes/view.js
+++ routes/view.js
@@ -2,25 +2,30 @@ const express = require('express');
 const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    // id: 59,
-    // name: 'ss',
-    // description: 'asxasxas',
-    // price: 55,
-    // stock: 111,
-    // is_approved: false,
-    // is_for_sale: false,
-    // user_id: null,
-    // thumbnail: '1620152227853',
-    // imge_id: null
+    
+    const mainQuery = `SELECT DISTINCT(products.*) ,favourites.id as fav_id, favourites.user_id as user_id ,favourites.product_id as fav_prod_id FROM products 
+    LEFT OUTER JOIN favourites ON favourites.product_id = products.id 
+    LEFT OUTER JOIN users ON users.id = favourites.user_id`;
+    if(req.query.minPrice && req.query.maxPrice){
+      const priceAppendQuery = mainQuery + ` Where price >= $1 AND price <= $2;`
+      
+      db.query(priceAppendQuery,[req.query.minPrice , req.query.maxPrice])
+    }
+    if(req.query.minPrice){
+      const priceAppendQuery = mainQuery + ` Where price <= $1`
+
+    }
+    
   db.query(`SELECT DISTINCT(products.*) ,favourites.id as fav_id, favourites.user_id as user_id ,favourites.product_id as fav_prod_id FROM products 
             LEFT OUTER JOIN favourites ON favourites.product_id = products.id 
             LEFT OUTER JOIN users ON users.id = favourites.user_id
              ;`) 
   .then(data => {
     const templateVars = {
-      data: data.rows
-    };
+      data:data.rows
 
+    };
+// console.log(templateVars);
         res.render("index",templateVars);
       })
       .catch(err => {
