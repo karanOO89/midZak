dd9d0e824541056c15d12d29324baf84a766e2d7
Zea <78378462+zeamaria@users.noreply.github.com>
2021-05-05 17:43:01 -0700

    do not use this branch!

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -7,12 +7,12 @@ VALUES ('Shirt', 'Androgenous' , 22, 10, true, true, 2, 'tshirt.jpg');
 INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
 VALUES ('Mask', 'Filter your air' , 50, 10, true, true, 2, 'mask.jpg');
 
-INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
-VALUES ('Macbook', 'Super fast' , 50, 10, true, true, 2, 'macbook.jpg');
+-- INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
+-- VALUES ('Macbook', 'Super fast' , 50, 10, true, true, 2, 'macbook.jpg');
 
 INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
 VALUES ('Hand sanitizer', 'Squeeky clean' , 50, 10, true, true, 2, 'sanitizer.jpg');
 
-INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
-VALUES ('Gloves', 'Extra protective' , 5, 10, true, true, 2, 'gloves.png');
+-- INSERT INTO products (name, description, price, stock, is_approved, is_for_sale, user_id, thumbnail)
+-- VALUES ('Gloves', 'Extra protective' , 5, 10, true, true, 2, 'gloves.png');
 
