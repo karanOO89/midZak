f10a96259665eec0d8bd81b5725a57340f009fd9
ArvinRad <radanarvin@gmail.com>
2021-05-07 00:38:31 +0000

    Updating the branch

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,5 +1,5 @@
-INSERT INTO messages (message, sender_id, receiver_id, product_id, thread_master_id, time)
-VALUES ('khkfkjd', 1, 2, 1, 1, 222222222);
+INSERT INTO messages (message, sender_id, product_id, thread_master_id, time)
+VALUES ('khkfkjd', 1, 2, 1, 22222);
 
 INSERT INTO thread_masters (product_id, user1_id, user2_id, is_dealt)
 VALUES (1, 2, 1, false);
