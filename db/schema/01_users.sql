00573c1ef45897284cd079b5754d47dc5d545dd6
karanOO89 <gk.987680@gmail.com>
2021-05-07 04:26:49 +0000

    messages issue fixed

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,19 +3,10 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255),
-  phone VARCHAR(255),
-  user_type_id INTEGER ,
-  address_id INTEGER
+  password VARCHAR(255)  
 );
 
-DROP TABLE IF EXISTS user_type CASCADE;
-CREATE TABLE user_type (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER,
-  user_type_name TEXT
-);
+
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO labber;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO labber;
